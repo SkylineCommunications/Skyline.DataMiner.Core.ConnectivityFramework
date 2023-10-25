6446f08aca8a29ade63e0d84830c948a3dcc032c
Jason Pitteman <97511712+JasonP-Skyline@users.noreply.github.com>
2023-10-25 09:31:52 +0200

    Added enums

---
+++ Protocol/Debug/DcfDebugLevel.cs
@@ -0,0 +1,31 @@
+﻿namespace Skyline.DataMiner.Core.ConnectivityFramework.Protocol.Debug
+{
+	//[DISCodeLibrary(Version = 1)]
+	public enum DcfDebugLevel
+	{
+		/// <summary>
+		/// Log all DCF Debug Info.
+		/// </summary>
+		All,
+
+		/// <summary>
+		/// Log only the Changes and the Setup
+		/// </summary>
+		Changes_And_Setup,
+
+		/// <summary>
+		/// Log only when something changes.
+		/// </summary>
+		Only_Changes,
+
+		/// <summary>
+		/// Log only the setup (buffers, startup checks).
+		/// </summary>
+		Only_Setup,
+
+		/// <summary>
+		/// No DCF Debug Info.
+		/// </summary>
+		None
+	}
+}
---
+++ Protocol/Logs/DcfLogType.cs
@@ -0,0 +1,26 @@
+﻿namespace Skyline.DataMiner.Core.ConnectivityFramework.Protocol.Logs
+{
+	//[DISCodeLibrary(Version = 1)]
+	public enum DcfLogType
+	{
+		/// <summary>
+		/// Indicates information about a change.
+		/// </summary>
+		Change,
+
+		/// <summary>
+		/// Indicates information about the setup.
+		/// </summary>
+		Setup,
+
+		/// <summary>
+		/// Indicates information about something staying the same.
+		/// </summary>
+		Same,
+
+		/// <summary>
+		/// Indicates default info.
+		/// </summary>
+		Info,
+	}
+}
